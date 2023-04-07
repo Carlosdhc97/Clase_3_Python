d157d2edc424607ea85f9c9b8c1983857a49811b
Carlosdhc97 <127318872+Carlosdhc97@users.noreply.github.com>
2023-04-07 15:42:42 -0400

    Create script#2.py

---
+++ script#2.py
@@ -0,0 +1,20 @@
+#Ordenae manualmente tres stacks
+import string
+    stack1 = [1,2,3]
+    stack2 = [5,4]
+    stack3 = []
+
+stack3.append(stack1.pop())
+stack3.append(stack1.pop())
+stack3.append(stack1.pop())
+stack1.append(stack2.pop())
+stack2.append(stack3.pop())
+stack1.append(stack3.pop())
+stack1.append(stack2.pop())
+stack2.append(stack3.pop())
+stack3.append(stack1.pop())
+
+
+print (stack1)
+print (stack2)
+print (stack3)
